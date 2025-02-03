03d20dd66e4ce9920e339cdcf639c9709452ca70
DevShree-31 <patelshreyansh376@gmail.com>
2025-02-03 16:21:52 +0530

    User model and its controllers for signIn and signUp CreatedSuccessfully

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './users/user.model';
+import { UsersModule } from './users/users.module';
+import { AppController,CatController } from './controllers/app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(), // Load .env variables
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Shreyansh_31',
+      database: 'todo',
+      models: [User], // Add all models here
+      autoLoadModels: true, // Automatically load models
+      synchronize: true, // Auto-sync models to DB (disable in production)
+    }),
+    UsersModule, // Register modules
+  ],
+  controllers: [AppController,CatController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService } from '../app.service';
 
 @Controller()
 export class AppController {
@@ -8,5 +8,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+}
+@Controller('cats')
+export class CatController{
+  constructor(private readonly appService: AppService) {}
+  @Get()
+  findAll():string{
+    return this.appService.findAll()
   }
 }
